04daea1be521bb410b2cf693d82cc0177c0ee69b
SeTzener <gaetanovf@gmail.com>
2024-05-10 00:13:49 +0200

    first push

---
+++ app/src/main/java/com/example/brewthings/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.brewthings
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.brewthings.screens.HomeScreen
+import com.example.brewthings.ui.theme.BrewthingsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BrewthingsTheme {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(colorScheme.background)
+                        .padding(24.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BrewthingsPreview() {
+    BrewthingsTheme {
+        Column(
+            Modifier.fillMaxSize()
+        ) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/brewthings/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.brewthings.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Darkmode_Primary = Color(0xBB86FCFF)
+val Darkmode_PrimaryVariant = Color(0x3700B3FF)
+val Darkmode_Secondary = Color(0x03DAC6FF)
+
+val Brightmode_Primary = Color(0xFF6650A4)
+val Brightmode_PrimaryVariant = Color(0xFF642B73)
+val Brightmode_Secondary = Color(0xFF625b71)
