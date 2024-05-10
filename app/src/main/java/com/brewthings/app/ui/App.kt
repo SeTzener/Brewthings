6e6fead47ff835002a679c8ac5abfa42c34d82be
wverdese <walt.verdese@gmail.com>
2024-05-11 00:54:37 +0200

    Insert Koin

---
+++ app/src/main/java/com/brewthings/app/ui/App.kt
@@ -0,0 +1,17 @@
+package com.brewthings.app.ui
+
+import android.app.Application
+import com.brewthings.app.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+            modules(appModule)
+        }
+    }
+}
