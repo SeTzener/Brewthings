600a91a3c19c2eb2253eccbf70b4e12c6b274075
SeTzener <gaetanovf@gmail.com>
2024-05-24 21:42:25 +0200

    Adds navigation classes

---
+++ app/src/main/java/com/brewthings/app/ui/screens/navigation/NavGraph.kt
@@ -0,0 +1,36 @@
+package com.brewthings.app.ui.screens.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.brewthings.app.ui.screens.graph.GraphScreen
+import com.brewthings.app.ui.screens.scanning.ScanningScreen
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+fun SetupNavGraph(
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Scanning.route
+    ) {
+        composable(
+            route = Screen.Scanning.route
+        ) {
+            ScanningScreen(
+                navController = navController,
+                viewModel = koinViewModel(),
+                openAppDetails = {},
+                showLocationSettings = {},
+                enableBluetooth = {}
+            )
+        }
+        composable(
+            route = Screen.Graph.route
+        ) {
+            GraphScreen(navController)
+        }
+    }
+}
