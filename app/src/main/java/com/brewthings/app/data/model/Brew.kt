8cb4638a8817aba2f22481701ca0f7b742107f88
SeTzener <gaetanovf@gmail.com>
2024-09-28 19:37:27 +0200

    Adding more values to the Brew Class (and fixing the tests)

--- app/src/main/java/com/brewthings/app/data/model/Brew.kt
+++ app/src/main/java/com/brewthings/app/data/model/Brew.kt
@@ -1,5 +1,7 @@
 package com.brewthings.app.data.model
 
+import com.brewthings.app.util.calculateABV
+import com.brewthings.app.util.datetime.TimeRange
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -7,4 +9,8 @@ data class Brew (
     val og: RaptPillData,
     val fgOrLast: RaptPillData,
     val isCompleted: Boolean,
+    val durationSinceOG: TimeRange = TimeRange(og.timestamp, fgOrLast.timestamp),
+    val abv: Float = calculateABV(og.gravity, fgOrLast.gravity)
 )
+
+
