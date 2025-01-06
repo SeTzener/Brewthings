454350c1aa62bd5acdf82cea8403c52603c17470
wverdese <walt.verdese@gmail.com>
2025-01-06 18:53:18 +0100

    Move feeding to calculations and refactor abv

--- app/src/main/java/com/brewthings/app/util/Calculations.kt
+++ app/src/main/java/com/brewthings/app/util/Calculations.kt
@@ -2,9 +2,12 @@ package com.brewthings.app.util
 
 import com.brewthings.app.data.model.RaptPillData
 
-fun calculateABV(og: Float, fg: Float, feedings: Float): Float {
-    if (og <= 1.0 || fg <= 1.0) return 0f
-    return (og.plus(feedings) - fg) * 131.25f
+fun calculateFeeding(previousGravity: Float, actualGravity: Float): Float =
+    actualGravity.minus(previousGravity)
+
+fun calculateABV(og: Float, fg: Float, feedings: List<Float>): Float? {
+    if (og <= 1.0 || fg <= 1.0) return null
+    return (og.sumAll(feedings) - fg) * 131.25f
 }
 
 fun calculateVelocity(previousData: RaptPillData?, fgData: RaptPillData): Float? {
