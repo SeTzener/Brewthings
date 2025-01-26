9cf0ae90a98e983b8325822e08bf6b03756af9c7
wverdese <walt.verdese@gmail.com>
2025-01-26 13:25:13 +0100

    Isolate router instantiation

--- app/src/main/java/com/brewthings/app/ui/screens/navigation/legacy/MainNavGraph.kt
+++ app/src/main/java/com/brewthings/app/ui/screens/navigation/legacy/MainNavGraph.kt
@@ -1,6 +1,7 @@
 package com.brewthings.app.ui.screens.navigation.legacy
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -11,30 +12,26 @@ import com.brewthings.app.ui.screens.pill.GraphScreen
 fun MainNavGraph(activityCallbacks: ActivityCallbacks) {
     val outerNavController = rememberNavController()
 
+    // This is the single entry point for the entire app where the router is created.
+    val router = remember(outerNavController) { Router(outerNavController) }
+
     NavHost(
         navController = outerNavController,
         startDestination = Destination.HOME,
     ) {
         composable(Destination.HOME) {
-            HomeScreen(
-                outerNavController = outerNavController,
-                activityCallbacks = activityCallbacks,
-            )
+            HomeScreen(router, activityCallbacks)
         }
 
         /* Entry point for supporting deep linking to a specific tab
         composable("${Destination.HOME}/{tab}") { backStackEntry ->
             val tab = backStackEntry.arguments?.getString("tab")
-            HomeScreen(
-                outerNavController = outerNavController,
-                activityCallbacks = activityCallbacks,
-                startDestination = tab,
-            )
+            HomeScreen(router, activityCallbacks, tab)
         }
         */
 
         composable(Destination.PILL_GRAPH) {
-            GraphScreen(Router(outerNavController))
+            GraphScreen(router)
         }
     }
 }
