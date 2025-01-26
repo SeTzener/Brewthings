a5f1a37b6165c131423e6a801b3f13c0b3fbc842
wverdese <walt.verdese@gmail.com>
2025-01-26 12:26:07 +0100

    Restore unwanted regression

--- app/src/main/java/com/brewthings/app/ui/theme/Theme.kt
+++ app/src/main/java/com/brewthings/app/ui/theme/Theme.kt
@@ -1,10 +1,17 @@
 package com.brewthings.app.ui.theme
 
+import android.os.Build
+import androidx.annotation.ChecksSdkIntAtLeast
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = Darkmode_Primary,
@@ -20,13 +27,11 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun BrewthingsTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    isDarkTheme: Boolean = isSystemInDarkTheme(),
+    isDynamicColoringEnabled: Boolean = true, //TODO(walt): wire it with a setting from the app
     content: @Composable () -> Unit,
 ) {
-    val colorScheme = when {
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = getColorScheme(isDynamicColoringEnabled, isDarkTheme)
 
     MaterialTheme(
         colorScheme = colorScheme,
@@ -34,3 +39,24 @@ fun BrewthingsTheme(
         content = content,
     )
 }
+
+@Composable
+private fun getColorScheme(isDynamicColoringEnabled: Boolean, isDarkTheme: Boolean): ColorScheme =
+    if (isDynamicColoringEnabled && isDynamicColoringSupported())
+        getDynamicColorScheme(isDarkTheme)
+    else
+        getAppColorScheme(isDarkTheme)
+
+@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.S)
+private fun isDynamicColoringSupported() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+
+@RequiresApi(Build.VERSION_CODES.S)
+@Composable
+private fun getDynamicColorScheme(isDarkTheme: Boolean): ColorScheme {
+    val context = LocalContext.current
+    return if (isDarkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+}
+
+@Composable
+private fun getAppColorScheme(isDarkTheme: Boolean): ColorScheme =
+    if (isDarkTheme) DarkColorScheme else LightColorScheme
