02aa0ff7c79c74f3c025e64066899cbfef1dde21
wverdese <walt.verdese@gmail.com>
2025-03-29 23:59:05 +0100

    Replace sweetness calculation

--- app/src/main/java/com/brewthings/app/ui/screen/composition/BrewCompositionViewModel.kt
+++ app/src/main/java/com/brewthings/app/ui/screen/composition/BrewCompositionViewModel.kt
@@ -6,6 +6,7 @@ import com.brewthings.app.ui.navigation.ParameterHolders.BrewComposition
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import org.koin.core.component.KoinComponent
+import kotlin.math.pow
 
 class BrewCompositionViewModel(
     brew: Brew = BrewComposition.brew ?: error("brew is required"),
@@ -21,7 +22,14 @@ class BrewCompositionViewModel(
 
 private val Brew.abvPercentage: Float get() = abv.coerceIn(0f, 100f)
 
+/**
+ * We calculate the residual sugars by converting the alcohol-adjusted final gravity into Brix.
+ * We use a standard SG to Brix conversion, and then multiply again by the FG, as described at
+ * [Vinolab](https://www.vinolab.hr/calculator/gravity-density-sugar-conversions-en19).
+ */
 private val Brew.sweetnessPercentage: Float get() {
-    val sweetness = 182.460f * (fgOrLast.gravity - 1f)
+    val fg = fgOrLast.gravity
+    val brix = 143.254f * fg.pow(3) - 648.670f * fg.pow(2) + 1125.805f * fg - 620.389f
+    val sweetness = brix * fg
     return sweetness.coerceIn(0f, 100f)
 }
